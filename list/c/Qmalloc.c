398830624eb1d101ccf168a4e4995394da869ad8
gen10 <gen10@Fake.com>
2024-04-19 14:02:37 +0800

    linux init

--- list/c/Qmalloc.c
+++ list/c/Qmalloc.c
@@ -2,3 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#if defined(__APPLE__)
+    #error "Apple does not support the brk function"
+#endif 
+
+
+int main(){
+    printf("hello");
+    return 0;
+}
